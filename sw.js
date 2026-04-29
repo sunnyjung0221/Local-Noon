c743d01a6717fc381db179cb8139034d6bce27f8
sunnyjung0221 <sunnyjung0221@pusan.ac.kr>
2026-04-29 19:02:58 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'solnce-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+// 설치 시 모든 파일 캐싱
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+// 오래된 캐시 삭제
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선, 실패 시 캐시
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
